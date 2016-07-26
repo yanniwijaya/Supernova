b2381ff995c0d9f8c3ec18f0e5b7cb80f2577c4a
yanniwijaya <yanni_wijaya@yahoo.com>
2016-07-26 12:03:13 +0700

    penambahan file test
    penambahan file test

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * Yanni Wijaya (YanniWijaya.com)
+ * @copyright Copyright (c) 2016 Yanni Wijaya
+ * @mail yanni.wijaya@gmail.com, yanni.wijaya@pln.co.id
+ */
+
+/**
+ * Description of test
+ *
+ * @author yanni.wijaya
+ */
+class test {
+    //put your code here
+}
