2195d5d1282dc6cba09ae2a13a19977968bc5463
yanni.wijaya <yanni.wijaya@Divsim-Yanni15.pusat.corp.pln.co.id>
2016-07-28 14:16:17 +0700

    Penambahan feature Create, Read, Update dan Delete untuk module pegawai.

--- module/Pegawai/Module.php
+++ module/Pegawai/Module.php
@@ -1,60 +1,64 @@
 <?php
 
 /*
- * Yanni Wijaya (YanniWijaya.com)
- * @copyright Copyright (c) 2016 Yanni Wijaya
- * @mail yanni.wijaya@gmail.com, yanni.wijaya@pln.co.id
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 
 namespace Pegawai;
-
-use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
-use Zend\ModuleManager\Feature\ConfigProviderInterface;
-
-use Pegawai\Model\Pegawai;
-use Pegawai\Model\PegawaiTable;
-use Zend\Db\ResultSet\ResultSet;
-use Zend\Db\TableGateway\TableGateway;
 
 /**
  * Description of Module
  *
  * @author yanni.wijaya
  */
-class Module implements AutoloaderProviderInterface, ConfigProviderInterface{
-    //put your code here
-    public function getAutoloaderConfig() {
-        return array(
-            'Zend\Loader\ClassMapAutoloader' => array(
-                __DIR__.'/autoload_classmap.php',
-            ),
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' .__NAMESPACE__,  
-                ),
-            ),
-        );
-    }
 
-    public function getConfig() {
-        return include __DIR__ . '/config/module.config.php';
-    }
-    
-    public function getServiceConfig() {
-        return array(
-            'factories' => array(
-                'Pegawai\Model\PegawaiTable' => function ($sm) {
-                    $tableGateway = $sm->get('PegawaiTableGateway');
-                    $table = new PegawaiTable($tableGateway);
-                    return $table;
-                },
-                'PegawaiTableGateway' => function ($sm) {
-                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
-                    $resultSetPrototype = new ResultSet();
-                    $resultSetPrototype->setArrayObjectPrototype(new Pegawai());
-                    return new TableGateway('pegawai', $dbAdapter, null, $resultSetPrototype);
-                },
-            ),
-        );
-    }
+use Zend\ModuleManager\Feature\AutoloaderProviderInterface;
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+ 
+use Pegawai\Model\Pegawai;
+use Pegawai\Model\PegawaiTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+ 
+class Module implements AutoloaderProviderInterface, ConfigProviderInterface
+ {
+     public function getAutoloaderConfig()
+     {
+         return array(
+             'Zend\Loader\ClassMapAutoloader' => array(
+                 __DIR__ . '/autoload_classmap.php',
+             ),
+             'Zend\Loader\StandardAutoloader' => array(
+                 'namespaces' => array(
+                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                 ),
+             ),
+         );
+     }
+
+     public function getConfig()
+     {
+         return include __DIR__ . '/config/module.config.php';
+     }
+     
+     public function getServiceConfig()
+     {
+         return array(
+             'factories' => array(
+                 'Pegawai\Model\PegawaiTable' =>  function($sm) {
+                     $tableGateway = $sm->get('PegawaiTableGateway');
+                     $table = new PegawaiTable($tableGateway);
+                     return $table;
+                 },
+                 'PegawaiTableGateway' => function ($sm) {
+                     $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                     $resultSetPrototype = new ResultSet();
+                     $resultSetPrototype->setArrayObjectPrototype(new Pegawai());
+                     return new TableGateway('pegawai', $dbAdapter, null, $resultSetPrototype);
+                 },
+             ),
+         );
+     }
 }
--- module/Pegawai/src/Pegawai/Controller/PegawaiController.php
+++ module/Pegawai/src/Pegawai/Controller/PegawaiController.php
@@ -39,15 +39,90 @@ class PegawaiController extends AbstractActionController{
     }
     
     public function addAction() {
-        
+         $form = new PegawaiForm();
+         $form->get('submit')->setValue('Add');
+         $request = $this->getRequest();
+         if ($request->isPost()) {
+             $pegawai = new Pegawai();
+             $form->setInputFilter($pegawai->getInputFilter());
+             $form->setData($request->getPost());
+
+             if ($form->isValid()) {
+                 $pegawai->exchangeArray($form->getData());
+                 $this->getPegawaiTable()->savePegawai($pegawai);
+
+                 // Redirect to list of pegawai
+                 return $this->redirect()->toRoute('pegawai');
+             }
+         }
+         return array('form' => $form);
     }
     
     public function editAction() {
-        
+        $id = (int) $this->params()->fromRoute('id', 0);
+         if (!$id) {
+             return $this->redirect()->toRoute('pegawai', array(
+                 'action' => 'add'
+             ));
+         }
+
+         // Get the Pegawai with the specified id.  An exception is thrown
+         // if it cannot be found, in which case go to the index page.
+         try {
+             $pegawai = $this->getPegawaiTable()->getPegawai($id);
+         }
+         catch (\Exception $ex) {
+             return $this->redirect()->toRoute('pegawai', array(
+                 'action' => 'index'
+             ));
+         }
+
+         $form  = new PegawaiForm();
+         $form->bind($pegawai);
+         $form->get('submit')->setAttribute('value', 'Edit');
+
+         $request = $this->getRequest();
+         if ($request->isPost()) {
+             $form->setInputFilter($pegawai->getInputFilter());
+             $form->setData($request->getPost());
+
+             if ($form->isValid()) {
+                 $this->getPegawaiTable()->savePegawai($pegawai);
+
+                 // Redirect to list of pegawai
+                 return $this->redirect()->toRoute('pegawai');
+             }
+         }
+
+         return array(
+             'id' => $id,
+             'form' => $form,
+         );
     }
     
     public function deleteAction() {
-        
+         $id = (int) $this->params()->fromRoute('id', 0);
+         if (!$id) {
+             return $this->redirect()->toRoute('pegawai');
+         }
+
+         $request = $this->getRequest();
+         if ($request->isPost()) {
+             $del = $request->getPost('del', 'No');
+
+             if ($del == 'Yes') {
+                 $id = (int) $request->getPost('id');
+                 $this->getPegawaiTable()->deletePegawai($id);
+             }
+
+             // Redirect to list of albums
+             return $this->redirect()->toRoute('pegawai');
+         }
+
+         return array(
+             'id'    => $id,
+             'pegawai' => $this->getPegawaiTable()->getPegawai($id)
+         );
     }
     
 }
